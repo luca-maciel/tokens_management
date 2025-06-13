5c13906c4262df6375f2ca3e1777cde8b57e0c44
Lucas Ferreira Maciel <lucas.ferreira@hrrbc.local>
2025-06-13 17:08:17 -0300

    changes

--- tokens_app/models.py
+++ tokens_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Token(models.Model):
+    nome_responsavel = models.CharField(max_length=125)
+    cpf_responsavel = models.CharField(max_length=11)
+    funcao_responsavel = models.CharField(max_length=125)
+    setor_responsavel = models.CharField(max_length=125)
+    telefone_responsavel = models.CharField(max_length=15)
+    serial = models.CharField(max_length=14)
+    data_solicitacao = models.DateField()
+    data_entrega = models.DateField()
+    data_modificacao = models.DateField(auto_now=True)
+    data_exclusao = models.DateField(null=True, blank=True)
+    observacao = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.nome_responsavel}"
+    
--- tokens_app/views.py
+++ tokens_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def lista_tokens(request):
+    return render(request, 'lista_tokens.html')
