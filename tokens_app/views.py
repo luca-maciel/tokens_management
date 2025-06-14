603a699cba93eef24011b529c028fa0b76f7d074
Lucas Maciel <lalucasforte56@gmail.com>
2025-06-13 23:35:25 -0300

    modal updates

--- tokens_app/views.py
+++ tokens_app/views.py
@@ -8,5 +8,21 @@ def home(request):
 
 def lista_tokens(request):
     tokens = Token.objects.all()
-    # print(tokens)
     return render(request, 'lista_tokens.html', {'tokens': tokens})
+
+def atualizar_token(request, token_id):
+    return HttpResponse("This view is not implemented yet.")
+    # token = Token.objects.get(id=token_id)
+    # if request.method == 'POST':
+    #     token.nome_responsavel = request.POST.get('nome_responsavel')
+    #     token.cpf_responsavel = request.POST.get('cpf_responsavel')
+    #     token.funcao_responsavel = request.POST.get('funcao_responsavel')
+    #     token.setor_responsavel = request.POST.get('setor_responsavel')
+    #     token.telefone_responsavel = request.POST.get('telefone_responsavel')
+    #     token.serial = request.POST.get('serial')
+    #     token.data_solicitacao = request.POST.get('data_solicitacao')
+    #     token.data_entrega = request.POST.get('data_entrega')
+    #     token.observacao = request.POST.get('observacao')
+    #     token.save()
+    #     return redirect('lista_tokens')
+    # return render(request, 'atualizar_token.html', {'token': token})
