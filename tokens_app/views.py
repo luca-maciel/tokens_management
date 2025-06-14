f894ab5dfda78a5bac6e00df9a14a9841bee31f4
Lucas Maciel <lalucasforte56@gmail.com>
2025-06-13 23:03:49 -0300

    updates

--- tokens_app/views.py
+++ tokens_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Token
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
 def lista_tokens(request):
-    return render(request, 'lista_tokens.html')
+    tokens = Token.objects.all()
+    # print(tokens)
+    return render(request, 'lista_tokens.html', {'tokens': tokens})
